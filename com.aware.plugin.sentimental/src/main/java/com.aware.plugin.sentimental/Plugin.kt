32c1eab2ad8dedf9fac811ae0ea1a341236844cf
Denzil Ferreira <ferreiradenzil@gmail.com>
2019-02-19 21:31:09 +0200

    - Code clean-up - Unified gradle wrapper in root, instead of submodules - WIP: new plugin "Sentimental", allowing the development of apps which can process in real-time text input from designated applications

---
+++ com.aware.plugin.sentimental/src/main/java/com.aware.plugin.sentimental/Plugin.kt
@@ -0,0 +1,90 @@
+package com.aware.plugin.sentimental
+
+import android.app.Service
+import android.content.ContentValues
+import android.content.Intent
+import android.util.Log
+import com.aware.Applications
+import com.aware.Aware
+import com.aware.providers.Applications_Provider
+import com.aware.providers.Keyboard_Provider
+import com.aware.utils.Aware_Plugin
+
+class Plugin : Aware_Plugin() {
+
+    companion object {
+        val PACKAGE_NAME = "packageName"
+        val TYPED_TEXT = "typedText"
+    }
+
+    var currentForeground = ""
+    var textBuffer = ""
+
+    override fun onCreate() {
+        super.onCreate()
+
+        TAG = "AWARE: Sentimental"
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+
+        if (PERMISSIONS_OK) {
+            if (Applications.isAccessibilityServiceActive(this)) {
+                Aware.startKeyboard(this)
+                Applications.setSensorObserver(object : Applications.AWARESensorObserver {
+                    override fun onCrash(data: ContentValues?) {}
+                    override fun onNotification(data: ContentValues?) {}
+                    override fun onBackground(data: ContentValues?) {}
+                    override fun onKeyboard(data: ContentValues?) {
+                        val keyboardInPackage = data!!.getAsString(Keyboard_Provider.Keyboard_Data.PACKAGE_NAME)
+                        if (keyboardInPackage == currentForeground) {
+                            textBuffer = data.getAsString(Keyboard_Provider.Keyboard_Data.CURRENT_TEXT)
+                        }
+                    }
+
+                    override fun onTouch(data: ContentValues?) {}
+                    override fun onForeground(data: ContentValues?) {
+                        val packagesOfInterest = Aware.getSetting(applicationContext, Settings.PLUGIN_SENTIMENTAL_PACKAGES).split(",")
+                        val currentApp = data!!.getAsString(Applications_Provider.Applications_Foreground.PACKAGE_NAME)
+                        if (packagesOfInterest.count() > 0 && packagesOfInterest.contains(currentApp)) {
+                            currentForeground = currentApp
+                        } else {
+                            if (!textBuffer.isEmpty() && awareSensor != null) {
+                                val contentValues = ContentValues()
+                                contentValues.put(Plugin.PACKAGE_NAME, currentForeground)
+                                contentValues.put(Plugin.TYPED_TEXT, textBuffer)
+                                (awareSensor as AWARESensorObserver).onTextContextChanged(contentValues)
+
+                                if (Aware.DEBUG)
+                                    Log.d("Sentimental", "onTextContextChanged -> ${contentValues.toString()}")
+                            }
+                            textBuffer = ""
+                            currentForeground = ""
+                        }
+                    }
+                })
+            }
+        }
+
+        return Service.START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private var awareSensor: AWARESensorObserver? = null
+
+    fun setSensorObserver(observer: AWARESensorObserver) {
+        awareSensor = observer
+    }
+
+    fun getSensorObserver(): AWARESensorObserver {
+        return awareSensor!!
+    }
+
+    interface AWARESensorObserver {
+        fun onTextContextChanged(data: ContentValues)
+    }
+}
