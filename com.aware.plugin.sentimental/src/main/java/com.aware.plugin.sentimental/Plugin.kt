53d060167e792e061b9029e22746212dde77a301
Denzil Ferreira <ferreiradenzil@gmail.com>
2019-02-28 15:32:51 +0200

    - Fix Sentimental plugin to make callback accessible - Added test for sentimental plugin

--- com.aware.plugin.sentimental/src/main/java/com.aware.plugin.sentimental/Plugin.kt
+++ com.aware.plugin.sentimental/src/main/java/com.aware.plugin.sentimental/Plugin.kt
@@ -2,8 +2,10 @@ package com.aware.plugin.sentimental
 
 import android.app.Service
 import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import com.aware.Applications
 import com.aware.Aware
 import com.aware.Aware_Preferences
@@ -11,20 +13,48 @@ import com.aware.providers.Applications_Provider
 import com.aware.providers.Keyboard_Provider
 import com.aware.utils.Aware_Plugin
 
-class Plugin : Aware_Plugin() {
+open class Plugin : Aware_Plugin() {
 
     companion object {
-        val PACKAGE_NAME = "packageName"
-        val TYPED_TEXT = "typedText"
+        const val PACKAGE_NAME = "packageName"
+        const val TYPED_TEXT = "typedText"
+
+        interface AWARESensorObserver {
+            fun onTextContextChanged(data: ContentValues)
+        }
+
+        var awareSensor: AWARESensorObserver ?= null
+
+        fun setSensorObserver(observer: AWARESensorObserver) {
+            awareSensor = observer
+        }
+
+        fun getSensorObserver(): AWARESensorObserver {
+            return awareSensor!!
+        }
     }
 
-    var currentForeground = ""
+    /**
+     * The package name of where the keyboard was interesting to track
+     */
+    var keyboardInApp = ""
+
+    /**
+     * List that contains the device's installed keyboard methods
+     */
+    var installedKeyboards = ""
+
+    /**
+     * Where we keep the buffer of written text
+     */
     var textBuffer = ""
 
     override fun onCreate() {
         super.onCreate()
-
         TAG = "AWARE: Sentimental"
+
+        val usingInput = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        installedKeyboards = usingInput.enabledInputMethodList.toString()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -36,38 +66,36 @@ class Plugin : Aware_Plugin() {
             Aware.setSetting(this, Settings.STATUS_PLUGIN_SENTIMENTAL, true)
 
             if (Applications.isAccessibilityServiceActive(this)) {
-
+                Aware.setSetting(this, Aware_Preferences.STATUS_APPLICATIONS, true)
                 Aware.startKeyboard(this)
                 Applications.setSensorObserver(object : Applications.AWARESensorObserver {
                     override fun onCrash(data: ContentValues?) {}
                     override fun onNotification(data: ContentValues?) {}
                     override fun onBackground(data: ContentValues?) {}
                     override fun onKeyboard(data: ContentValues?) {
-                        val keyboardInPackage = data!!.getAsString(Keyboard_Provider.Keyboard_Data.PACKAGE_NAME)
-                        if (keyboardInPackage == currentForeground) {
+                        val packagesOfInterest = Aware.getSetting(applicationContext, Settings.PLUGIN_SENTIMENTAL_PACKAGES).split(",")
+                        if (packagesOfInterest.contains(data!!.getAsString(Keyboard_Provider.Keyboard_Data.PACKAGE_NAME))) {
+                            keyboardInApp = data!!.getAsString(Keyboard_Provider.Keyboard_Data.PACKAGE_NAME)
                             textBuffer = data.getAsString(Keyboard_Provider.Keyboard_Data.CURRENT_TEXT)
                         }
                     }
 
                     override fun onTouch(data: ContentValues?) {}
                     override fun onForeground(data: ContentValues?) {
-                        val packagesOfInterest = Aware.getSetting(applicationContext, Settings.PLUGIN_SENTIMENTAL_PACKAGES).split(",")
+
                         val currentApp = data!!.getAsString(Applications_Provider.Applications_Foreground.PACKAGE_NAME)
-                        if (packagesOfInterest.count() > 0 && packagesOfInterest.contains(currentApp)) {
-                            currentForeground = currentApp
-                        } else {
-                            if (!textBuffer.isEmpty() && awareSensor != null) {
+                        if (installedKeyboards.contains(currentApp)) return //we ignore foreground package of keyboard input
 
-                                val contentValues = ContentValues()
-                                contentValues.put(Plugin.PACKAGE_NAME, currentForeground)
-                                contentValues.put(Plugin.TYPED_TEXT, textBuffer)
+                        if (!textBuffer.isEmpty() && currentApp != keyboardInApp) { //we were using an app of interest and changed app
 
-                                (awareSensor as AWARESensorObserver).onTextContextChanged(contentValues)
+                            val contentValues = ContentValues()
+                            contentValues.put(Plugin.PACKAGE_NAME, keyboardInApp)
+                            contentValues.put(Plugin.TYPED_TEXT, textBuffer)
 
-                                if (DEBUG) Log.d("Sentimental", "onTextContextChanged -> ${contentValues.toString()}")
-                            }
+                            Plugin.awareSensor?.onTextContextChanged(contentValues)
+
                             textBuffer = ""
-                            currentForeground = ""
+                            keyboardInApp = ""
                         }
                     }
                 })
@@ -79,22 +107,8 @@ class Plugin : Aware_Plugin() {
 
     override fun onDestroy() {
         super.onDestroy()
-
+        Aware.setSetting(this, Aware_Preferences.STATUS_APPLICATIONS, false)
         Aware.setSetting(this, Settings.STATUS_PLUGIN_SENTIMENTAL, false)
         Aware.stopKeyboard(this)
-    }
-
-    private var awareSensor: AWARESensorObserver? = null
-
-    fun setSensorObserver(observer: AWARESensorObserver) {
-        awareSensor = observer
-    }
-
-    fun getSensorObserver(): AWARESensorObserver {
-        return awareSensor!!
-    }
-
-    interface AWARESensorObserver {
-        fun onTextContextChanged(data: ContentValues)
     }
 }
